bfb2d13a07b2ab4cfd25a031b5f1274a3217d2e4
nuzcraft <nuzcraft@gmail.com>
2022-07-16 06:30:55 -0500

    adding some creature sprite indexes

--- release/js/vars.js
+++ release/js/vars.js
@@ -4,6 +4,11 @@ const uiWidth = 4;
 
 const sprsht_idx_creatures = 0;
 const spr_idx_knight = 0;
+const spr_idx_snake = 225;
+const spr_idx_blue_bird = 232;
+const spr_idx_black_bird = 233;
+const spr_idx_worm = 330;
+const spr_idx_worm_eyeless = 331;
 
 const sprsht_idx_world = 1;
 const spr_idx_floor = 5;
