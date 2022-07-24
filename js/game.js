2e4e5fc1379f873b0bac678722be682adbe308fa
nuzcraft <nuzcraft@gmail.com>
2022-07-23 20:46:54 -0500

    readme update

--- js/game.js
+++ js/game.js
@@ -208,8 +208,8 @@ function showTitle(){
 
   gameState = "title"
 
-  drawText("SUPER", 40, true, canvas.height/2 - 110, "white");
-  drawText("BROUGH BROS.", 70, true, canvas.height / 2 - 50, "white");
+  drawText("MONSTER HUNTER", 40, true, canvas.height/2 - 110, "white");
+  drawText("WONDERLAND.", 70, true, canvas.height / 2 - 50, "white");
 }
 
 function startGame(){
