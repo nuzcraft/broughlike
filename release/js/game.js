bbddcd4d660193850a37b4c580012d44385bcf98
nuzcraft <nuzcraft@gmail.com>
2022-07-23 20:39:17 -0500

    drawing text, finishing part 5

--- release/js/game.js
+++ release/js/game.js
@@ -58,6 +58,8 @@ function draw() {
     }
 
     player.draw();
+
+    drawText("Level: " + level, 30, false, 40, "white");
   }
 }
 
@@ -205,6 +207,9 @@ function showTitle(){
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   gameState = "title"
+
+  drawText("SUPER", 40, true, canvas.height/2 - 110, "white");
+  drawText("BROUGH BROS.", 70, true, canvas.height / 2 - 50, "white");
 }
 
 function startGame(){
@@ -222,4 +227,17 @@ function startLevel(playerHp){
   player.hp = playerHp;
 
   randomPassableTile().replace(Exit);
+}
+
+function drawText(text, size, centered, textY, color){
+  ctx.fillStyle = color;
+  ctx.font = size + "px monospace";
+  let textX;
+  if(centered){
+    textX = (canvas.width - ctx.measureText(text).width)/2;
+  } else {
+    textX = canvas.width - uiWidth * tileSize + 25;
+  }
+
+  ctx.fillText(text, textX, textY);
 }
